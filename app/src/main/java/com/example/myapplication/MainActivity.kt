e07a2324836e28ef99b68ee2dcf9f72a56ef9dc7
entaku <entaku19890818@gmail.com>
2024-03-23 14:23:36 +0900

    add motion

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.AudioPlayerActivity
 import com.example.myapplication.ui.AudioRecorderActivity
+import com.example.myapplication.ui.MotionActivity
 import com.example.myapplication.ui.TextToSpeechActivity
 
 class MainActivity : ComponentActivity() {
@@ -47,6 +48,11 @@ class MainActivity : ComponentActivity() {
                 context.startActivity(Intent(context, TextToSpeechActivity::class.java))
             }) {
                 Text(text = "Go to TextToSpeechActivity")
+            }
+            Button(onClick = {
+                context.startActivity(Intent(context, MotionActivity::class.java))
+            }) {
+                Text(text = "Go to MotionActivity")
             }
         }
     }
